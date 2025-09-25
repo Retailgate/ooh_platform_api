da38f776e4ffd48295d9591bc87ee6a1648a1525
ISTI-Developers <164320922+ISTI-Developers@users.noreply.github.com>
2025-09-25 16:15:54 +0800

    onsite changes

--- src/routes/utasi.route.ts
+++ src/routes/utasi.route.ts
@@ -36,6 +36,7 @@ router.put("/train/assets/edit/:id", TrainController.updateTrainAsset);
 // External Routes
 router.get("/train/external/specs/:asset_id", ExternalController.getExternalAssetSpecs);
 router.post("/train/external/addViaduct", ExternalController.addViaductAssetAndSpecs);
+router.put("/train/external/updateExternal/:id", ExternalController.updateExternalBooking);
 router.delete("/train/external/deleteViaduct/:id/:spec_id", ExternalController.deleteViaductAssetAndSpecs);
 
 export const UTASIRoute = router;
